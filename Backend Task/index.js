17ab7083fd6cddeb317e1a0b169957559c71d932
Bhanu Prakash <pbhanu.1994@gmail.com>
2024-09-14 20:46:47 +1000

    Implemented Update Route for updating the users details by userId

--- Backend Task/index.js
+++ Backend Task/index.js
@@ -30,6 +30,19 @@ app.get("/users/:id", (req, res) => {
   res.json(user);
 });
 
+// Update (PUT) - Update an existing user by id
+app.put("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  const user = users.find((u) => u.id === parseInt(userId));
+  if (!user) {
+    return res.status(404).json({ error: `User not found with id: ${userId}` });
+  }
+  const { name, email } = req.body;
+  user.name = name;
+  user.email = email;
+  res.json(user);
+});
+
 // Starting the Server
 const PORT = 3000;
 app.listen(PORT, () => {
