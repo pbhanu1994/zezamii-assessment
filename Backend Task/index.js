d45b5e5bf77aadb3322a406cdbc0d5cd73b2fd3d
Bhanu Prakash <pbhanu.1994@gmail.com>
2024-09-14 20:31:11 +1000

    Implemented Post route for adding new user with id, name & email

--- Backend Task/index.js
+++ Backend Task/index.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+// In-memory storage (array) for user data
+let users = [];
+let userId = 1;
+
+/* CRUD Operation */
+// Create (POST) - Add a new user
+app.post("/users", (req, res) => {
+  const { name, email } = req.body;
+  const newUser = { id: userId++, name, email };
+  users.push(newUser);
+  res.status(201).json(newUser);
 });
 
 const PORT = 3000;
