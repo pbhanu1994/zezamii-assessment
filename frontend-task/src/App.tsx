a7aea13351213d5a8b2e5059a94426051d850bf9
Bhanu Prakash <pbhanu.1994@gmail.com>
2024-09-15 22:53:50 +1000

    Initial commit - frontend test

---
+++ frontend-task/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Zezamii Test App</header>
+    </div>
+  );
+};
+
+export default App;
