cd43ca1cf3c11289631b0343a9de9861f0730b49
Bhanu Prakash <pbhanu.1994@gmail.com>
2024-09-16 18:39:18 +1000

    Implemented the skeleton loading for products

---
+++ frontend-task/src/components/Products/ProductsLoading.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Grid, Card, CardContent, Skeleton } from "@mui/material";
+
+export const ProductsLoading = () => {
+  const skeletonArray = Array.from(new Array(8));
+
+  return (
+    <React.Fragment>
+      {skeletonArray.map((_, index) => (
+        <Grid item xs={12} sm={6} md={4} key={index}>
+          <Card
+            sx={{
+              maxWidth: 345,
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Skeleton variant="rectangular" width="100%" height={200} />
+            <CardContent>
+              <Skeleton variant="text" width="60%" height={30} />
+              <Skeleton variant="text" width="40%" height={20} />
+              <Skeleton variant="text" width="100%" height={60} />
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </React.Fragment>
+  );
+};
