8b60f900def72094db7cc84efcbb97e2d5f5a163
Bhanu Prakash <pbhanu.1994@gmail.com>
2024-09-17 01:19:20 +1000

    Testing Products - Fetching & Displaying products

--- frontend-task/src/components/Products/Products.test.tsx
+++ frontend-task/src/components/Products/Products.test.tsx
@@ -1,18 +1,40 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { ProductsLoading } from "./ProductsLoading";
+import Products from "./Products";
+import getProducts from "../../services/products";
+import { mockProducts } from "../../mocks/products";
+
+// Mocking the getProducts service
+jest.mock("../../services/products");
 
 describe("Products Component", () => {
+  beforeEach(() => {
+    (getProducts as jest.Mock).mockResolvedValue(mockProducts);
+  });
+
   // Testing the Products - skeleton loading component
   test("renders loading state initially", () => {
     render(<ProductsLoading />);
 
-    // Checking if the button is rendered
+    // Checking if the skeleton loading cards are rendered
     const skeletonLoadingCard1 = screen.getByLabelText(/productLoading-1/i);
     const skeletonLoadingCard2 = screen.getByLabelText(/productLoading-2/i);
 
     expect(skeletonLoadingCard1).toBeInTheDocument();
     expect(skeletonLoadingCard2).toBeInTheDocument();
+  });
+
+  // Testing the Products - displaying products
+  test("fetches and displays products", async () => {
+    render(<Products />);
+
+    // Wait for the products to be fetched and rendered
+    await waitFor(() => {
+      expect(screen.getByText("Product 1")).toBeInTheDocument();
+      expect(screen.getByText("Product 2")).toBeInTheDocument();
+      expect(screen.getByText("Product 3")).toBeInTheDocument();
+    });
   });
 });
---
+++ frontend-task/src/mocks/products.ts
@@ -0,0 +1,24 @@
+// Mock data
+export const mockProducts = [
+  {
+    id: 1,
+    title: "Product 1",
+    description: "Description for Product 1",
+    price: 29.99,
+    thumbnail: "https://via.placeholder.com/200",
+  },
+  {
+    id: 2,
+    title: "Product 2",
+    description: "Description for Product 2",
+    price: 49.99,
+    thumbnail: "https://via.placeholder.com/200",
+  },
+  {
+    id: 3,
+    title: "Product 3",
+    description: "Description for Product 3",
+    price: 73.99,
+    thumbnail: "https://via.placeholder.com/200",
+  },
+];
